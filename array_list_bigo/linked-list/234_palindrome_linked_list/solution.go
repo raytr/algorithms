dd7402f98431c7ed12c334ea9e41a403de5813c0
Ray Tran <nghiatran.ray@gmail.com>
2022-08-21 21:53:34 +0700

    dfs

--- array_list_bigo/linked-list/234_palindrome_linked_list/solution.go
+++ array_list_bigo/linked-list/234_palindrome_linked_list/solution.go
@@ -5,15 +5,21 @@ package palindrome_linked_list
 
     avoid to use extra memmory
 
+	clarify:
+    what is the range of the number of nodes in this linked list
+    what is range of node's value
+
+    avoid to use extra memmory
+
     step 1: find the center node
         use 2 pointers, fast and slow
-        when fash move 2 note, slow move 1 node. => when fast.Next  == nil => centre node is slow
+        when fast move 2 note, slow move 1 node. => when fast.Next  == nil => centre node is slow
 
    step 2: revert ll half behind
    step 3: compare util meet a centre node
 
-	Time complexity: O(n)
-	Space complexity: O(1)
+    Time complexity: O(n)
+    Space complexity: O(1)
 */
 
 type ListNode struct {
@@ -21,36 +27,48 @@ type ListNode struct {
 	Next *ListNode
 }
 
+/*
+
+
+ */
+
 func isPalindrome(head *ListNode) bool {
-	res := true
-	p1 := head
-	p2 := endOfFirstHaft(head)
-	p2 = reverseList(p2)
-	for p2 != nil {
-		if p2.Val != p1.Val {
-			res = false
-			break
-		}
-		p1 = p1.Next
-		p2 = p2.Next
+	//eage case
+	if head.Next == nil {
+		return true
 	}
-	return res
+
+	centerNode := findCenterNode(head)
+
+	//revert linked list from half to end
+	revertedLL := revertLinkedList(centerNode)
+
+	//compare node by node until end of revertedLL
+	for revertedLL != nil {
+		if revertedLL.Val != head.Val {
+			return false
+		}
+		head = head.Next
+		revertedLL = revertedLL.Next
+	}
+	return true
 }
 
-func endOfFirstHaft(head *ListNode) *ListNode {
-	slow, fast := head, head
+func findCenterNode(head *ListNode) *ListNode {
+	fast := head.Next.Next
+	slow := head.Next
 	for fast != nil && fast.Next != nil {
-		slow = slow.Next
 		fast = fast.Next.Next
+		slow = slow.Next
 	}
 	return slow
 }
 
-func reverseList(head *ListNode) *ListNode {
+func revertLinkedList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
-	p := reverseList(head.Next)
+	p := revertLinkedList(head.Next)
 	head.Next.Next = head
 	head.Next = nil
 	return p
