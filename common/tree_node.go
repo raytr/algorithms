dc7c343d60982881778e590071c6276aa4cdc97f
nghiatran <nghia.tran-vn@klikdokter.com>
2023-09-26 15:04:45 +0700

    add common module

---
+++ common/tree_node.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func constructTree(arr []*int, i int) *TreeNode {
+
+	if i < len(arr) && arr[i] != nil {
+		node := &TreeNode{Val: *arr[i]}
+		node.Left = constructTree(arr, 2*i+1)
+		node.Right = constructTree(arr, 2*i+2)
+		return node
+	}
+	return nil
+}
+
+func IntegerStringToTreeNode(arrStr string) (*TreeNode, error) {
+	arrStr = arrStr[1 : len(arrStr)-1]
+
+	arr := make([]*int, 0)
+
+	nums := strings.Split(arrStr, ",")
+	for _, nStr := range nums {
+		if nStr == "" || nStr == "null" || nStr == "nil" {
+			arr = append(arr, nil)
+		} else {
+			num, err := strconv.Atoi(nStr)
+			if err != nil {
+				return nil, err
+			}
+			arr = append(arr, &num)
+		}
+	}
+
+	result := constructTree(arr, 0)
+	return result, nil
+
+}
